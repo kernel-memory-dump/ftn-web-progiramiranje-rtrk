680345f79d7b7beae0cc30c9f1ad2dac6f3df3b4
student <student@domain.local>
2017-03-15 17:33:31 +0100

    Osnove op

---
+++ vezbe2/PurposeLife42/src/rs/ac/uns/ftn/krt/model/Torba.java
@@ -0,0 +1,50 @@
+package rs.ac.uns.ftn.krt.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Torba {
+	
+	private List<String> sminka = new ArrayList<>();
+	
+	public boolean hasNesto(String nesto){
+		return sminka.contains(nesto);
+	}
+
+	public boolean dodaj(String nesto){
+		return sminka.add(nesto);
+	}
+	
+	
+	public Torba() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((sminka == null) ? 0 : sminka.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Torba other = (Torba) obj;
+		if (sminka == null) {
+			if (other.sminka != null)
+				return false;
+		} else if (!sminka.equals(other.sminka))
+			return false;
+		return true;
+	}
+	
+
+}
