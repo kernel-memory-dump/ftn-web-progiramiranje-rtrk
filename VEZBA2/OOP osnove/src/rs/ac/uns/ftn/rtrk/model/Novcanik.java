15d424271e3e625838dfc706020875d81f8aed5e
PETAR PERIC <pera@pera.com>
2017-03-17 17:28:32 +0100

    ADSDASDAS

---
+++ VEZBA2/OOP osnove/src/rs/ac/uns/ftn/rtrk/model/Novcanik.java
@@ -0,0 +1,17 @@
+package rs.ac.uns.ftn.rtrk.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Novcanik {
+	
+	// ctrl+shift+o
+	List<String> sadrzaj = new ArrayList<>();
+	
+	
+	// da li ima, da se doda i da se izbaci
+	public boolean hasItem(String item) {
+		return sadrzaj.contains(item);
+	}
+
+}
